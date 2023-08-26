79f9b14874165e7fbb2e54b70675ade1df35c924
zhilin1112 <chriskong1112@gmail.com>
2023-08-26 15:46:22 +0800

    Slower taskflow

--- TaskFlow.cpp
+++ TaskFlow.cpp
@@ -8,21 +8,27 @@
 #include <vector>
 #include "taskflow/taskflow/taskflow.hpp"
 
-bool sort_by_dist(const double* v1, const double* v2);
+//bool sort_by_dist(const double* v1, const double* v2);
+
 class Knn {
 private:
     int neighbours_number;
+
 public:
     Knn(int k) : neighbours_number(k) {}
+
     int predict_class(double* dataset[], const double* target, int dataset_size, int feature_size) {
         double* distances[3];
         int zeros_count = 0;
         int ones_count = 0;
         int prediction = -1;
+
         distances[0] = new double[dataset_size];
         distances[1] = new double[dataset_size];
         distances[2] = new double[dataset_size];
+
         get_knn(dataset, target, distances, dataset_size, feature_size);
+
         for (int i = 0; i < dataset_size; i++) {
             if (distances[1][i] == 0) {
                 zeros_count += 1;
@@ -31,17 +37,21 @@ public:
                 ones_count += 1;
             }
         }
+
         if (zeros_count > ones_count) {
             prediction = 0;
         }
         else {
             prediction = 1;
         }
+
         delete[] distances[0];
         delete[] distances[1];
         delete[] distances[2];
+
         return prediction;
     }
+
 private:
     double euclidean_distance(const double* x, const double* y, int feature_size) {
         double l2 = 0.0;
@@ -50,6 +60,7 @@ private:
         }
         return std::sqrt(l2);
     }
+
     void get_knn(double* x[], const double* y, double* distances[3], int dataset_size, int feature_size) {
         int count = 0;
         for (int i = 0; i < dataset_size; i++) {
@@ -65,20 +76,26 @@ private:
             });
     }
 };
-class ParallelKnn {
+
+class TaskflowKnn {
 private:
     int neighbours_number;
+
 public:
-    ParallelKnn(int k) : neighbours_number(k) {}
-    int parallel_predict_class(double* dataset[], const double* target, int dataset_size, int feature_size) {
+    TaskflowKnn(int k) : neighbours_number(k) {}
+
+    int predict_class(double* dataset[], const double* target, int dataset_size, int feature_size) {
         double* distances[3];
         int zeros_count = 0;
         int ones_count = 0;
         int prediction = -1;
+
         distances[0] = new double[dataset_size];
         distances[1] = new double[dataset_size];
         distances[2] = new double[dataset_size];
-        parallel_get_knn(dataset, target, distances, dataset_size, feature_size);
+
+        get_knn(dataset, target, distances, dataset_size, feature_size);
+
         for (int i = 0; i < dataset_size; i++) {
             if (distances[1][i] == 0) {
                 zeros_count += 1;
@@ -87,17 +104,21 @@ public:
                 ones_count += 1;
             }
         }
+
         if (zeros_count > ones_count) {
             prediction = 0;
         }
         else {
             prediction = 1;
         }
+
         delete[] distances[0];
         delete[] distances[1];
         delete[] distances[2];
+
         return prediction;
     }
+
 private:
     double euclidean_distance(const double* x, const double* y, int feature_size) {
         double l2 = 0.0;
@@ -106,30 +127,43 @@ private:
         }
         return std::sqrt(l2);
     }
-    void parallel_get_knn(double* x[], const double* y, double* distances[3], int dataset_size, int feature_size) {
+
+    void get_knn(double* x[], const double* y, double* distances[3], int dataset_size, int feature_size) {
+        int count = 0;
+
         tf::Executor executor;
         tf::Taskflow taskflow;
+
         for (int i = 0; i < dataset_size; i++) {
             if (x[i] == y) continue; // do not use the same point
-            auto task = taskflow.emplace([this, i, x, y, distances, feature_size]() {
+            auto task = taskflow.emplace([this, i, x, y, distances, feature_size, &count]() {
                 distances[0][i] = this->euclidean_distance(y, x[i], feature_size);
                 distances[1][i] = x[i][0]; // Store outcome label
                 distances[2][i] = i; // Store index
+                count++;
                 });
         }
+
         executor.run(taskflow).wait();
+
         std::sort(distances[2], distances[2] + dataset_size, [distances](int i, int j) {
             return distances[0][i] < distances[0][j];
             });
+
+        std::cout << "Number of euclidean run: " << count << std::endl;
     }
+
 };
+
 bool sort_by_dist(const double* v1, const double* v2) {
     return v1[0] < v2[0];
 }
+
 std::vector<double> parseLine(const std::string& line) {
     std::vector<double> row;
     std::istringstream iss(line);
     std::string value;
+
     while (std::getline(iss, value, ',')) {
         try {
             double num = std::stod(value);
@@ -139,8 +173,10 @@ std::vector<double> parseLine(const std::string& line) {
             std::cerr << "Invalid data in CSV: " << value << std::endl;
         }
     }
+
     return row;
 }
+
 int main() {
     std::string filename = "diabetes_binary.csv";
 
@@ -178,42 +214,57 @@ int main() {
 
     std::cout << "Number of records: " << index << std::endl;
 
-    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    //Taskflow Knn
+#pragma region TaskflowKnn
+    std::cout << "\n\Taskflow KNN: " << std::endl;
+    std::chrono::steady_clock::time_point taskflowBegin = std::chrono::steady_clock::now();
 
-    //Knn knn(3); // Use K=3
-    //int prediction = knn.predict_class(dataset, target, dataset_size, feature_size);
+    TaskflowKnn taskflowknn(3); // Use K=3
+    int taskflowPrediction = taskflowknn.predict_class(dataset, target, dataset_size, feature_size);
+    std::cout << "Taskflow Prediction: " << taskflowPrediction << std::endl;
 
-    ParallelKnn parallelKnn(3); // Use K=3
-    int parallel_prediction = parallelKnn.parallel_predict_class(dataset, target, dataset_size, feature_size);
-
-    //std::cout << "Serial Prediction: " << prediction << std::endl;
-    std::cout << "Parallel Prediction: " << parallel_prediction << std::endl;
-
-    //if (prediction == 0) {
-    //    std::cout << "Serial Predicted class: Negative" << std::endl;
-    //}
-    //else if (prediction == 1) {
-    //    std::cout << "Serial Predicted class: Prediabetes or Diabetes" << std::endl;
-    //}
-    //else {
-    //    std::cout << "Serial Prediction could not be made." << std::endl;
-    //}
-
-    if (parallel_prediction == 0) {
-        std::cout << "Parallel Predicted class: Negative" << std::endl;
-
+    if (taskflowPrediction == 0) {
+        std::cout << "Predicted class: Negative" << std::endl;
     }
-    else if (parallel_prediction == 1) {
-        std::cout << "Parallel Predicted class: Prediabetes or Diabetes" << std::endl;
+    else if (taskflowPrediction == 1) {
+        std::cout << "Predicted class: Prediabetes or Diabetes" << std::endl;
     }
     else {
-        std::cout << "Parallel Prediction could not be made." << std::endl;
+        std::cout << "Prediction could not be made." << std::endl;
     }
-    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    std::cout << "Serial Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[µs]" << std::endl;
+
+    std::chrono::steady_clock::time_point taskflowEnd = std::chrono::steady_clock::now();
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(taskflowEnd - taskflowBegin).count() << "[µs]" << std::endl;
+#pragma endregion
+
+    //Knn
+#pragma region Knn
+    std::cout << "\n\nKNN: " << std::endl;
+    std::chrono::steady_clock::time_point knnBegin = std::chrono::steady_clock::now();
+    Knn knn(3); // Use K=3
+
+    int prediction = knn.predict_class(dataset, target, dataset_size, feature_size);
+    std::cout << "Prediction: " << prediction << std::endl;
+
+    if (prediction == 0) {
+        std::cout << "Predicted class: Negative" << std::endl;
+    }
+    else if (prediction == 1) {
+        std::cout << "Predicted class: Prediabetes or Diabetes" << std::endl;
+    }
+    else {
+        std::cout << "Prediction could not be made." << std::endl;
+    }
+
+    std::chrono::steady_clock::time_point knnEnd = std::chrono::steady_clock::now();
+    std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(knnEnd - knnBegin).count() << "[µs]" << std::endl;
+#pragma endregion
+
+
     // Deallocate memory for dataset
     for (int i = 0; i < dataset_size; i++) {
         delete[] dataset[i];
     }
+
     return 0;
 }
