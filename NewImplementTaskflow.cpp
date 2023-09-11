ec558cba55c113a58970ea344ea539f197e6cfe4
zhilin1112 <chriskong1112@gmail.com>
2023-09-11 08:13:00 +0800

    Update

--- NewImplementTaskflow.cpp
+++ NewImplementTaskflow.cpp
@@ -243,6 +243,153 @@ private:
 	//}
 };
 
+class Knn {
+private:
+	int neighbours_number;
+
+public:
+	Knn(int k) : neighbours_number(k) {}
+
+	int predict_class(double* dataset[], const double* target, int dataset_size, int feature_size) {
+		double* distances[3];
+		int zeros_count = 0;
+		int ones_count = 0;
+
+		// Allocate memory for distances and index order
+		distances[0] = new double[dataset_size];
+		distances[1] = new double[dataset_size];
+		distances[2] = new double[dataset_size];
+
+		get_knn(dataset, target, distances, dataset_size, feature_size);
+
+		serial_merge_sort(distances, 0, dataset_size - 1);
+
+		/*for (int i = 0; i < 10; i++) {
+			std::cout << distances[0][i] << "," << distances[1][i] << "," << distances[2][i] << std::endl;
+		}*/
+
+		// Count label occurrences in the K nearest neighbors
+		for (int i = 0; i < neighbours_number; i++) {
+			if (distances[1][i] == 0) {
+				zeros_count += 1;
+				std::cout << "0: " << distances[0][i] << "," << distances[2][i] << std::endl;
+			}
+			else if (distances[1][i] == 1) {
+				ones_count += 1;
+				std::cout << "1: " << distances[0][i] << "," << distances[2][i] << std::endl;
+			}
+		}
+
+		int prediction = (zeros_count > ones_count) ? 0 : 1;
+
+		//Clean up memory
+		delete[] distances[0];
+		delete[] distances[1];
+		delete[] distances[2];
+
+		return prediction;
+	}
+
+private:
+
+	static void merge(double** distances, int low, int middle, int high) {
+		int n1 = middle - low + 1;
+		int n2 = high - middle;  //high = middle + 1 
+
+		double* left[3];
+		double* right[3];
+
+		// Create temporary arrays
+		for (int i = 0; i < 3; i++) {
+			left[i] = new double[n1];
+			right[i] = new double[n2];
+		}
+
+		// Copy data to temporary arrays left[] and right[]
+		for (int i = 0; i < n1; i++) {
+			for (int j = 0; j < 3; j++) {
+				left[j][i] = distances[j][low + i];
+			}
+		}
+		for (int i = 0; i < n2; i++) {
+			for (int j = 0; j < 3; j++) {
+				right[j][i] = distances[j][middle + 1 + i];
+			}
+		}
+
+		// Merge the temporary arrays back into distances[3]
+		int i = 0, j = 0, k = low;
+		while (i < n1 && j < n2) {
+			if (left[0][i] <= right[0][j]) {
+				for (int x = 0; x < 3; x++) {
+					distances[x][k] = left[x][i];
+				}
+				i++;
+			}
+			else {
+				for (int x = 0; x < 3; x++) {
+					distances[x][k] = right[x][j];
+				}
+				j++;
+			}
+			k++;
+		}
+
+		// Copy the remaining elements of left[], if any
+		while (i < n1) {
+			for (int x = 0; x < 3; x++) {
+				distances[x][k] = left[x][i];
+			}
+			i++;
+			k++;
+		}
+
+		// Copy the remaining elements of right[], if any
+		while (j < n2) {
+			for (int x = 0; x < 3; x++) {
+				distances[x][k] = right[x][j];
+			}
+			j++;
+			k++;
+		}
+
+		// Clean up temporary arrays
+		for (int x = 0; x < 3; x++) {
+			delete[] left[x];
+			delete[] right[x];
+		}
+	}
+
+	static void serial_merge_sort(double** distances, int low, int high) {
+		if (low < high) {
+			int middle = low + (high - low) / 2;
+			serial_merge_sort(distances, low, middle);
+			serial_merge_sort(distances, middle + 1, high);
+			merge(distances, low, middle, high);
+		}
+	}
+
+	double euclidean_distance(const double* x, const double* y, int feature_size) {
+		double l2 = 0.0;
+		for (int i = 1; i < feature_size; i++) {
+			l2 += std::pow((x[i] - y[i]), 2);
+		}
+		return std::sqrt(l2);
+	}
+
+	void get_knn(double* x[], const double* y, double* distances[3], int dataset_size, int feature_size) {
+		int count = 0;
+		for (int i = 0; i < dataset_size; i++) {
+			if (x[i] == y) continue; // do not use the same point
+			distances[0][count] = this->euclidean_distance(y, x[i], feature_size);
+			distances[1][count] = x[i][0]; // Store outcome label
+			distances[2][count] = i; // Store index
+			count++;
+		}
+		std::cout << "Number of euclidean run:" << count << std::endl;
+	}
+};
+
 vector<double> parseLine(const string& line) {
 	vector<double> row;
 	istringstream iss(line);
@@ -301,6 +448,7 @@ int main() {
 
 	steady_clock::time_point begin = steady_clock::now();
 
+#pragma region TasflowKnn
 	TaskflowKnn tfknn(3); // Use K=3, with 4 threads
 	int taskFlowPrediction = tfknn.predict_class(dataset, target, dataset_size, feature_size);
 
@@ -318,6 +466,32 @@ int main() {
 	}
 
 	cout << "Time difference = " << duration_cast<microseconds>(end - begin).count() << "[ s]" << endl;
+
+#pragma endregion
+
+
+	//Knn
+#pragma region Knn
+	std::cout << "\n\nKNN: " << std::endl;
+	std::chrono::steady_clock::time_point knnBegin = std::chrono::steady_clock::now();
+	Knn knn(3); // Use K=3
+
+	int prediction = knn.predict_class(dataset, target, dataset_size, feature_size);
+	std::cout << "Prediction: " << prediction << std::endl;
+
+	if (prediction == 0) {
+		std::cout << "Predicted class: Negative" << std::endl;
+	}
+	else if (prediction == 1) {
+		std::cout << "Predicted class: Prediabetes or Diabetes" << std::endl;
+	}
+	else {
+		std::cout << "Prediction could not be made." << std::endl;
+	}
+
+	std::chrono::steady_clock::time_point knnEnd = std::chrono::steady_clock::now();
+	std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(knnEnd - knnBegin).count() << "[µs]" << std::endl;
+#pragma endregion
 
 	return 0;
 }
